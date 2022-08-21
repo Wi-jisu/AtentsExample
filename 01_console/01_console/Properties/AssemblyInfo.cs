d6dfa476ad64e3b34ab51d65d2ab46a54cdab0cb
Wi-jisu <111379239+Wi-jisu@users.noreply.github.com>
2022-08-21 17:10:13 +0900

    ???
    ???

--- 01_console/01_console/Properties/AssemblyInfo.cs
+++ 01_console/01_console/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("1eda52ce-10e8-45d3-b63d-39b116eba8be")]
+[assembly: Guid("1cda1062-8067-4b61-a2f1-ce85f4d151eb")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
